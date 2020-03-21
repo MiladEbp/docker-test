01af920a3f25a5a206d4899c282782a3c1e09d3c
milad_ebp <ebrahimpoumilad74@gmail.com>
2020-03-22 01:10:51 +0430

    #commit

--- api/users.js
+++ api/users.js
@@ -28,7 +28,7 @@ router.get("/api/users/:id?", (req, res) => __awaiter(void 0, void 0, void 0, fu
                 result = { result: -1, message: "کاربری وجود ندارد!" };
             }
             else {
-                result = { result: 0, data: getUser, name: "milad" };
+                result = { result: 0, data: getUser };
             }
         }
         else if (Object.keys(query).length !== 0) {
